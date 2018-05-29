32a3620ef82d9ef41ff53dadab1b137d9d6e5507
Andrew <aomsis97@mail.ru>
2018-05-29 21:10:26 +0600

    Changed iOS native view to native module

--- example/App.js
+++ example/App.js
@@ -5,99 +5,28 @@
  */
 
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  NativeModules,
+  TouchableOpacity,
+} from 'react-native';
 import FoxitPDFView from 'react-native-foxit-pdf';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
 type Props = {};
 export default class App extends Component<Props> {
+  onPress() {
+    FoxitPDFView.openPDF(require('./sample.pdf'));
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-        <FoxitPDFView
-          style={{ flex: 1 }}
-          password={''}
-          source={require('./sample.pdf')}
-          enableTopToolbar={true}
-          extensionConfig={{
-            readingBookmark: true,
-            thumbnail: true,
-            attachment: true,
-            signature: true,
-            search: true,
-            pageNavigation: true,
-            form: true,
-            encryption: true,
-            tools: {
-              Insert: true,
-              Eraser: true,
-              Line: true,
-              Arrow: true,
-              Selection: true,
-              Underline: true,
-              Replace: true,
-              StrikeOut: true,
-              Note: true,
-              Highlight: true,
-              Squiggly: true,
-              Oval: true,
-              Freetext: true,
-              Stamp: true,
-              Rectangle: true,
-              Pencil: true,
-              Textbox: true,
-              Polygon: true,
-              Cloud: true,
-              Image: true,
-              Distance: true,
-            },
-          }}
-          topToolbarConfig={{
-            ItemMore: false,
-            ItemBack: false,
-            ItemBookmark: false,
-            ItemSearch: false,
-          }}
-          bottomToolbarConfig={{
-            ItemAnnot: false,
-            ItemPanel: false,
-            ItemReadmore: false,
-            ItemSignature: false,
-          }}
-          panelConfig={{
-            readingBookmark: false,
-            outline: false,
-            annotation: true,
-            attachments: true,
-          }}
-          viewSettingsConfig={{
-            singlePage: false,
-            continuousPage: false,
-            thumbnail: true,
-            autoBrightness: true,
-            night: false,
-            reflow: false,
-            screenLock: true,
-            crop: true,
-            panAndZoom: true,
-            facing: true,
-          }}
-          viewMoreConfig={{
-            groupFile: true,
-            groupFormConfig: {
-              itemResetForm: true,
-            },
-          }}
-        />
+        <TouchableOpacity onPress={this.onPress}>
+          <Text>Open PDF</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -109,15 +38,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
--- lib/ios/FoxitPDF/PDFManager.m
+++ lib/ios/FoxitPDF/PDFManager.m
@@ -7,38 +7,405 @@
 //
 
 #import <Foundation/Foundation.h>
-#import "RNTPDF.h"
 #import <FoxitRDK/FSPDFViewControl.h>
-#import <React/RCTBridge.h>
+#import <React/RCTBridgeModule.h>
 #import <React/RCTViewManager.h>
+#import "uiextensions/UIExtensionsManager.h"
 
-@interface RNTPDFManager : RCTViewManager
+@interface RNTPDFManager : NSObject <RCTBridgeModule, UIExtensionsManagerDelegate, IDocEventListener>
 @end
 
-@implementation RNTPDFManager
+@implementation RNTPDFManager {
+    FSPDFViewCtrl* pdfViewCtrl;
+    UIExtensionsManager* extensionsManager;
+    NSDictionary* _topToolbarConfig;
+    NSDictionary* _bottomToolbarConfig;
+    NSDictionary* _panelConfig;
+    NSDictionary* _viewSettingsConfig;
+    NSDictionary* _viewMoreConfig;
+    NSArray *topToolbarVerticalConstraints;
+    
+    UINavigationController *rootViewController;
+    UIViewController *pdfViewController;
+}
 
-RCT_EXPORT_MODULE()
+RCT_EXPORT_MODULE(PDFManager)
 
 @synthesize bridge = _bridge;
 
-- (UIView *)view
-{
-  return [[RNTPDF alloc] initWithEventDispatcher:self.bridge.eventDispatcher];
+RCT_EXPORT_METHOD(openPDF:(NSString *)src
+                  password:(NSString *)password
+                  extensionConfig:(NSDictionary *)extensionConfig
+                  enableTopToolbar:(BOOL)enableTopToolbar
+                  enableBottomToolbar:(BOOL)enableBottomToolbar
+                  topToolbarConfig:(NSDictionary *)topToolbarConfig
+                  bottomToolbarConfig:(NSDictionary *)bottomToolbarConfig
+                  panelConfig:(NSDictionary *)panelConfig
+                  viewSettingsConfig:(NSDictionary *)viewSettingsConfig
+                  viewMoreConfig:(NSDictionary *)viewMoreConfig) {
+    pdfViewCtrl = [[FSPDFViewCtrl alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    [pdfViewCtrl registerDocEventListener:self];
+    
+    //    [pdfViewCtrl openDoc:pdfPath password:nil completion:nil];
+    //    pdfViewCtrl.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    pdfViewController = [[UIViewController alloc] init];
+    pdfViewController.automaticallyAdjustsScrollViewInsets = NO;
+    pdfViewController.view = pdfViewCtrl;
+    rootViewController = [[UINavigationController alloc] initWithRootViewController:pdfViewController];
+    rootViewController.navigationBarHidden = YES;
+    
+    if ( extensionConfig != NULL) {
+        NSData* configData = [NSJSONSerialization dataWithJSONObject:@{
+                                                                       @"modules": extensionConfig
+                                                                       }
+                                                             options:NSJSONWritingPrettyPrinted
+                                                               error: nil];
+        
+        extensionsManager = [[UIExtensionsManager alloc] initWithPDFViewControl:pdfViewCtrl configuration:configData];
+    } else {
+        extensionsManager = [[UIExtensionsManager alloc] initWithPDFViewControl:pdfViewCtrl];
+    }
+    
+    extensionsManager.delegate = self;
+    
+
+    [extensionsManager enableBottomToolbar:enableBottomToolbar];
+    [extensionsManager enableTopToolbar:enableTopToolbar];
+    
+    [self setPanelConfig:panelConfig];
+    [self setViewMoreConfig:viewMoreConfig];
+    [self setViewSettingsConfig:viewSettingsConfig];
+    [self setTopToolbarConfig:topToolbarConfig];
+    [self setBottomToolbarConfig:bottomToolbarConfig];
+    
+    pdfViewCtrl.extensionsManager = extensionsManager;
+    [self wrapTopToolbar];
+    topToolbarVerticalConstraints = @[];
+    
+    NSData *data = [NSData dataWithContentsOfURL:[NSURL URLWithString:src]];
+    FSPDFDoc *pdfdoc  = [[FSPDFDoc alloc] initWithMemory:data];
+    [pdfdoc load:nil];
+    //FSPDFDoc* pdfdoc = [[FSPDFDoc alloc] initWithHandler:(nonnull id<FSFileReadCallback>)];
+    // Load the unencrypted document content.
+    //if(e_errSuccess != [pdfdoc load:nil]) {
+    //  return; }
+    
+    // Set the document to view control.
+    [pdfViewCtrl setDoc:pdfdoc];
+    
+    [[[UIApplication sharedApplication].delegate window].rootViewController presentViewController:rootViewController animated:YES completion:^{
+        
+    }];
 }
 
-- (dispatch_queue_t)methodQueue
+//panelConfig
+- (void) setPanelConfig: (NSDictionary *) config
 {
-  return dispatch_get_main_queue();
+    _panelConfig = config;
+    
+    if (_panelConfig != NULL) {
+        id val = [_panelConfig objectForKey:@"readingBookmark"] ;
+        
+        if (val != NULL) {
+            [extensionsManager.panelController setPanelHidden:[val boolValue] type:FSPanelTypeReadingBookmark];
+        }
+        
+        val = [_panelConfig objectForKey:@"outline"] ;
+        
+        if (val != NULL) {
+            [extensionsManager.panelController setPanelHidden:[val boolValue] type:FSPanelTypeOutline];
+        }
+        
+        val = [_panelConfig objectForKey:@"annotation"] ;
+        
+        if (val != NULL) {
+            [extensionsManager.panelController setPanelHidden:[val boolValue] type:FSPanelTypeAnnotation];
+        }
+        
+        val = [_panelConfig objectForKey:@"attachments"] ;
+        
+        if (val != NULL) {
+            [extensionsManager.panelController setPanelHidden:[val boolValue] type:FSPanelTypeAttachment];
+        }
+    }
 }
 
-RCT_EXPORT_VIEW_PROPERTY(src, NSString);
-RCT_EXPORT_VIEW_PROPERTY(password, NSString);
-RCT_EXPORT_VIEW_PROPERTY(extensionConfig, NSDictionary);
-RCT_EXPORT_VIEW_PROPERTY(enableTopToolbar, BOOL);
-RCT_EXPORT_VIEW_PROPERTY(enableBottomToolbar, BOOL);
-RCT_EXPORT_VIEW_PROPERTY(topToolbarConfig, NSDictionary);
-RCT_EXPORT_VIEW_PROPERTY(bottomToolbarConfig, NSDictionary);
-RCT_EXPORT_VIEW_PROPERTY(panelConfig, NSDictionary);
-RCT_EXPORT_VIEW_PROPERTY(viewSettingsConfig, NSDictionary);
-RCT_EXPORT_VIEW_PROPERTY(viewMoreConfig, NSDictionary);
+//viewMoreConfig
+- (void) setViewMoreConfig: (NSDictionary *) config
+{
+    _viewMoreConfig = config;
+    
+    if (_viewMoreConfig != NULL) {
+        id val = [_viewMoreConfig objectForKey:@"groupFile"] ;
+        
+        if (val != NULL) {
+            [extensionsManager setToolbarItemHiddenWithTag:1 hidden:[val boolValue]];
+        }
+        
+        val = [_viewMoreConfig objectForKey:@"groupProject"] ;
+        
+        if (val != NULL) {
+            [extensionsManager setToolbarItemHiddenWithTag:4 hidden:[val boolValue]];
+        }
+        
+        val = [_viewMoreConfig objectForKey:@"groupForm"] ;
+        
+        if (val != NULL) {
+            [extensionsManager setToolbarItemHiddenWithTag:7 hidden:[val boolValue]];
+        }
+        
+        NSDictionary *groupFileConfig = [_viewMoreConfig objectForKey:@"groupFileConfig"];
+        
+        // TAG_GROUP_FILE Config
+        if (groupFileConfig != NULL) {
+            val = [groupFileConfig objectForKey:@"itemFileInfo"] ;
+            
+            if (val != NULL) {
+                [extensionsManager setToolbarItemHiddenWithTag:10 hidden:[val boolValue]];
+            }
+            
+            val = [groupFileConfig objectForKey:@"itemReduceFileSize"] ;
+            
+            if (val != NULL) {
+                [extensionsManager setToolbarItemHiddenWithTag:11 hidden:[val boolValue]];
+            }
+            
+            val = [groupFileConfig objectForKey:@"itemWirelessPrint"] ;
+            
+            if (val != NULL) {
+                [extensionsManager setToolbarItemHiddenWithTag:14 hidden:[val boolValue]];
+            }
+            
+            val = [groupFileConfig objectForKey:@"itemCrop"] ;
+            
+            if (val != NULL) {
+                [extensionsManager setToolbarItemHiddenWithTag:16 hidden:[val boolValue]];
+            }
+        }
+        
+        NSDictionary *groupProjectConfig = [_viewMoreConfig objectForKey:@"groupProjectConfig"];
+        // TAG_GROUP_PROJECT
+        if (groupProjectConfig != NULL) {
+            val = [groupProjectConfig objectForKey:@"itemPassword"] ;
+            
+            if (val != NULL) {
+                [extensionsManager setToolbarItemHiddenWithTag:17 hidden:[val boolValue]];
+            }
+        }
+        
+        NSDictionary *groupFormConfig = [_viewMoreConfig objectForKey:@"groupFormConfig"];
+        // TAG_GROUP_FORM CONFIG
+        if (groupFormConfig != NULL) {
+            val = [groupFormConfig objectForKey:@"itemResetForm"] ;
+            
+            if (val != NULL) {
+                [extensionsManager setToolbarItemHiddenWithTag:24 hidden:[val boolValue]];
+            }
+            
+            val = [groupFormConfig objectForKey:@"itemImportForm"] ;
+            
+            if (val != NULL) {
+                [extensionsManager setToolbarItemHiddenWithTag:25 hidden:[val boolValue]];
+            }
+            
+            val = [groupFormConfig objectForKey:@"itemExportForm"] ;
+            
+            if (val != NULL) {
+                [extensionsManager setToolbarItemHiddenWithTag:26 hidden:[val boolValue]];
+            }
+        }
+    }
+}
+
+//view Settings config
+- (void) setViewSettingsConfig: (NSDictionary *) config
+{
+    _viewSettingsConfig = config;
+    
+    if (_viewSettingsConfig != NULL) {
+        id val = [_viewSettingsConfig objectForKey:@"singlePage"] ;
+        if (val != NULL) {
+            [extensionsManager.settingBar setItem:SINGLE hidden:[val boolValue]];
+        }
+        
+        val = [_viewSettingsConfig objectForKey:@"continuousPage"] ;
+        if (val != NULL) {
+            [extensionsManager.settingBar setItem:CONTINUOUS hidden:[val boolValue]];
+        }
+        
+        val = [_viewSettingsConfig objectForKey:@"thumbnail"] ;
+        if (val != NULL) {
+            [extensionsManager.settingBar setItem:THUMBNAIL hidden:[val boolValue]];
+        }
+        
+        val = [_viewSettingsConfig objectForKey:@"autoBrightness"] ;
+        if (val != NULL) {
+            [extensionsManager.settingBar setItem:BRIGHTNESS hidden:[val boolValue]];
+        }
+        
+        val = [_viewSettingsConfig objectForKey:@"night"] ;
+        if (val != NULL) {
+            [extensionsManager.settingBar setItem:NIGHTMODE hidden:[val boolValue]];
+        }
+        
+        val = [_viewSettingsConfig objectForKey:@"reflow"] ;
+        if (val != NULL) {
+            [extensionsManager.settingBar setItem:REFLOW hidden:[val boolValue]];
+        }
+        
+        val = [_viewSettingsConfig objectForKey:@"screenLock"] ;
+        if (val != NULL) {
+            [extensionsManager.settingBar setItem:LOCKSCREEN hidden:[val boolValue]];
+        }
+        
+        val = [_viewSettingsConfig objectForKey:@"crop"] ;
+        if (val != NULL) {
+            [extensionsManager.settingBar setItem:CROPPAGE hidden:[val boolValue]];
+        }
+        
+        val = [_viewSettingsConfig objectForKey:@"panAndZoom"] ;
+        if (val != NULL) {
+            [extensionsManager.settingBar setItem:PANZOOM hidden:[val boolValue]];
+        }
+        
+        val = [_viewSettingsConfig objectForKey:@"facing"] ;
+        if (val != NULL) {
+            [extensionsManager.settingBar setItem:DOUBLEPAGE hidden:[val boolValue]];
+        }
+        
+    }
+}
+
+- (void) setTopToolbarConfig: (NSDictionary *) config
+{
+    _topToolbarConfig = config;
+    
+    if (_topToolbarConfig != NULL) {
+        id val = [_topToolbarConfig objectForKey:@"ItemMore"] ;
+        
+        if (val != NULL) {
+            [extensionsManager setToolbarItemHiddenWithTag:FS_TOPBAR_ITEM_MORE_TAG hidden:[val boolValue]];
+        }
+        
+        val = [_topToolbarConfig objectForKey:@"ItemBack"] ;
+        
+        if (val != NULL) {
+            [extensionsManager setToolbarItemHiddenWithTag:FS_TOPBAR_ITEM_BACK_TAG hidden:[val boolValue]];
+        }
+        
+        val = [_topToolbarConfig objectForKey:@"ItemBookmark"] ;
+        
+        if (val != NULL) {
+            [extensionsManager setToolbarItemHiddenWithTag:FS_TOPBAR_ITEM_BOOKMARK_TAG hidden:[val boolValue]];
+        }
+        
+        val = [_topToolbarConfig objectForKey:@"ItemSearch"] ;
+        
+        if (val != NULL) {
+            [extensionsManager setToolbarItemHiddenWithTag:FS_TOPBAR_ITEM_SEARCH_TAG hidden:[val boolValue]];
+        }
+    }
+}
+
+- (void) setBottomToolbarConfig: (NSDictionary *) config
+{
+    _bottomToolbarConfig = config;
+    
+    if (_bottomToolbarConfig != NULL) {
+        id val = [_bottomToolbarConfig objectForKey:@"ItemAnnot"] ;
+        
+        if (val != NULL) {
+            [extensionsManager setToolbarItemHiddenWithTag:FS_BOTTOMBAR_ITEM_ANNOT_TAG hidden:[val boolValue]];
+        }
+        
+        val = [_bottomToolbarConfig objectForKey:@"ItemPanel"] ;
+        
+        if (val != NULL) {
+            [extensionsManager setToolbarItemHiddenWithTag:FS_BOTTOMBAR_ITEM_PANEL_TAG hidden:[val boolValue]];
+        }
+        
+        val = [_bottomToolbarConfig objectForKey:@"ItemReadmore"] ;
+        
+        if (val != NULL) {
+            [extensionsManager setToolbarItemHiddenWithTag:FS_BOTTOMBAR_ITEM_READMODE_TAG hidden:[val boolValue]];
+        }
+        
+        val = [_bottomToolbarConfig objectForKey:@"ItemSignature"] ;
+        
+        if (val != NULL) {
+            [extensionsManager setToolbarItemHiddenWithTag:FS_BOTTOMBAR_ITEM_SIGNATURE_TAG hidden:[val boolValue]];
+        }
+    }
+}
+
+// MARK: - UIExtensionsManagerDelegate
+- (void)uiextensionsManager:(UIExtensionsManager *)uiextensionsManager setTopToolBarHidden:(BOOL)hidden {
+    UIToolbar *topToolbar = extensionsManager.topToolbar;
+    UIView *topToolbarWrapper = topToolbar.superview;
+    id topGuide = pdfViewController.topLayoutGuide;
+    assert(topGuide);
+    
+    [pdfViewCtrl removeConstraints:topToolbarVerticalConstraints];
+    if (!hidden) {
+        NSMutableArray *contraints = @[].mutableCopy;
+        [contraints addObjectsFromArray:
+         [NSLayoutConstraint constraintsWithVisualFormat:@"V:[topGuide]-0-[topToolbar(44)]"
+                                                 options:0
+                                                 metrics:nil
+                                                   views:NSDictionaryOfVariableBindings(topToolbar, topGuide)]];
+        [contraints addObjectsFromArray:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[topToolbarWrapper]"
+                                                                                options:0
+                                                                                metrics:nil
+                                                                                  views:NSDictionaryOfVariableBindings(topToolbarWrapper)]];
+        topToolbarVerticalConstraints = contraints;
+        
+    } else {
+        topToolbarVerticalConstraints = [NSLayoutConstraint constraintsWithVisualFormat:@"V:[topToolbarWrapper]-0-[topGuide]"
+                                                                                options:0
+                                                                                metrics:nil
+                                                                                  views:NSDictionaryOfVariableBindings(topToolbarWrapper, topGuide)];
+    }
+    [pdfViewCtrl addConstraints:topToolbarVerticalConstraints];
+    [UIView animateWithDuration:0.3
+                     animations:^{
+                         [pdfViewCtrl layoutIfNeeded];
+                     }];
+}
+
+- (void)wrapTopToolbar {
+    // let status bar be translucent. top toolbar is top layout guide (below status bar), so we need a wrapper to cover the status bar.
+    UIToolbar *topToolbar = extensionsManager.topToolbar;
+    [topToolbar setTranslatesAutoresizingMaskIntoConstraints:NO];
+    
+    UIView *topToolbarWrapper = [[UIToolbar alloc] init];
+    [topToolbarWrapper setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [pdfViewCtrl insertSubview:topToolbarWrapper belowSubview:topToolbar];
+    [topToolbarWrapper addSubview:topToolbar];
+    
+    [pdfViewCtrl addConstraints:
+     [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[topToolbarWrapper]-0-|"
+                                             options:0
+                                             metrics:nil
+                                               views:NSDictionaryOfVariableBindings(topToolbarWrapper)]];
+    [topToolbarWrapper addConstraints:
+     [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[topToolbar]-0-|"
+                                             options:0
+                                             metrics:nil
+                                               views:NSDictionaryOfVariableBindings(topToolbar)]];
+    [topToolbarWrapper addConstraints:
+     [NSLayoutConstraint constraintsWithVisualFormat:@"V:[topToolbar]-0-|"
+                                             options:0
+                                             metrics:nil
+                                               views:NSDictionaryOfVariableBindings(topToolbar)]];
+}
+
+// MARK: - IDocEventListener
+- (void)onDocOpened:(FSPDFDoc *)document error:(int)error {
+    
+}
+
+- (void)onDocClosed:(FSPDFDoc *)document error:(int)error {
+    [rootViewController dismissViewControllerAnimated:YES completion:nil];
+}
+
 @end
