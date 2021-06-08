e6cabf5a6894d9e462c96f4289e05215f903e71e
xinqiang_song <xinqiang_song@foxitsoftware.cn>
2021-06-08 15:32:14 +0800

    refs#SDKRD-6326 Update demo code for support Android Foxit SDK PDF 8.0

--- example/android/app/src/main/java/com/example/MainActivity.java
+++ example/android/app/src/main/java/com/example/MainActivity.java
@@ -1,23 +1,15 @@
 package com.example;
 
-import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.support.annotation.Nullable;
-
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
-    }
 
-    /**
-     * Returns the name of the main component registered from JavaScript.
-     * This is used to schedule rendering of the component.
-     */
-    @Override
-    protected String getMainComponentName() {
-        return "Example";
-    }
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "example";
+  }
 }
